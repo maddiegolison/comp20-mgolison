d25be3c5c625464fd6c1572a494c8504c0284bed
maddiegolison <madeleine.golison@tufts.edu>
2015-02-28 12:23:17 -0500

    experimenting with javascript

--- mspacman/game.js
+++ mspacman/game.js
@@ -1,1 +1,18 @@
-// Replace this with your code...
+/* Lab 5 */
+
+function init() {
+ canvas = document.getElementById('game_canvas');
+        if (canvas.getContext) {
+            ctx = canvas.getContext('2d');
+        }
+        else {
+            alert('Sorry, canvas is not supported on your browser!');
+        }
+        var img = new Image();
+        img.src = "pacman10-hp-sprite.png";
+        img.addEventListener("load", function() {
+        	ctx.drawImage(img,0,0);
+  		// statement to draw the blue board
+  		// statement to draw Ms. Pac-Man
+		}, false);
+}
