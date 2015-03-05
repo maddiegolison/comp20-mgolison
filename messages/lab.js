e6e45786a6d5c9b23def14f540cd6834071202c8
maddiegolison <madeleine.golison@tufts.edu>
2015-03-05 12:57:27 -0500

    wrote parseData

--- messages/lab.js
+++ messages/lab.js
@@ -13,11 +13,11 @@ function parse() {
 
 function parseData() {
 
-	if (request.readyState() == 4){
+	if (request.readyState == 4){
 		messagesDiv = document.getElementById("messages");
 		converted = JSON.parse(request.responseText);
 		for (i = 0; i < converted.length; i++) {
-			messagesDiv.innerHTML += "<p>" + converted[i]['content'];
+			messagesDiv.innerHTML += "<p>" + converted[i]['content'] + "</p>";
 		}
 	}
 }
